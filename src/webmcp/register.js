6e2976b9d9ee2a8d4c2ffe53e92ba7b079cc4bcc
Shivam Arora <174376166+Shiv-aurora@users.noreply.github.com>
2026-09-02 18:54:05 -0400

    Register review-safe WebMCP actions

--- src/webmcp/register.js
+++ src/webmcp/register.js
@@ -1,18 +1,19 @@
 import { buildResearchWebMCPTools, READ_ONLY_WEBMCP_TOOL_NAMES } from "./researchTools";
+import { ACTION_WEBMCP_TOOL_NAMES, buildResearchWebMCPActionTools } from "./actionTools";
 
 export function registerResearchWebMCPTools(editorState, ownerDocument = document) {
   const modelContext = ownerDocument?.modelContext;
   const controller = new AbortController();
   const registrationState = {
     supported: Boolean(modelContext?.registerTool),
-    toolNames: [...READ_ONLY_WEBMCP_TOOL_NAMES],
+    toolNames: [...READ_ONLY_WEBMCP_TOOL_NAMES, ...ACTION_WEBMCP_TOOL_NAMES],
     registration: null,
   };
   editorState.webmcp = registrationState;
 
   if (!registrationState.supported) return () => controller.abort();
 
-  const tools = buildResearchWebMCPTools(editorState);
+  const tools = [...buildResearchWebMCPTools(editorState), ...buildResearchWebMCPActionTools(editorState)];
   registrationState.registration = Promise.allSettled(tools.map((tool) => modelContext.registerTool(tool, { signal: controller.signal }))).then(
     (results) => {
       registrationState.results = results;
